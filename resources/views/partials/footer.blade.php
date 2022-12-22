7bc540181c7090dfbd00a0be886fd0a1f528e96f
Jucimara Pereira <maricimara98@outlook.com>
2022-12-22 16:42:58 -0300

    particionando o projeto

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <div>
+        <p>Aprendendo Laravel Framework</p>
+        <p><a href="http://www.laravel.com.br" target="_blank">Laravel Site</a></p>
+    </div>
+    <div>
+        <p>Prof. Me. Jorge Luís Gregório</p>
+        <p><a href="https://www.jlgregorio.com.br" target="_blank">Meu Site Oficial</a></p>
+    </div>
+</footer>
---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        {{-- Links para o cadastro --}}
+        <li><a href="vehicles">Início</a></li>
+        <li><a href="vehicles/create">Cadastro de Veículos</a></li>
+    </ul>
+</nav>
